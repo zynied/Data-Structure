ce85ed37b827910b72ed436565b42caf4290127a
zynie <2598049398@qq.com>
2020-11-03 17:36:34 +0800

    【20-11-03新增】

---
+++ src/main/java/com/nzy/datastructure/array/SparseArray.java
@@ -0,0 +1,19 @@
+package com.nzy.datastructure.array;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        Solution solution = new SparseArray().new Solution();
+
+    }
+     class Solution{
+        //普通数组转化为稀疏数组
+         public Integer[][] toSparse(Integer[][] arr){
+            return new Integer[][]{};
+         }
+         //稀疏数组转化为普通数组
+         public Integer[][] toArray(Integer[][] spa){
+             return new Integer[][]{};
+         }
+
+    }
+}
